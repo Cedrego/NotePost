5b155ffb67f76722f34ac49e3b616c4fe710ba88
Franco Da Costa <da-costaruiz@hotmail.com>
2025-06-21 21:21:58 -0300

    extra

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { BarraLateralComponent } from '../barra-lateral/barra-lateral.component'; //Borrar a Futuro es Testeo
+import { PostListComponent } from '../post-list/post-list.component';
 import { SessionService } from '../services/session.service';// <-- importando servicio de sesión
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
   //imports: [],
-  imports: [BarraLateralComponent, CommonModule], //Borrar a Futuro es Testeo
+  imports: [BarraLateralComponent, CommonModule, PostListComponent], //Borrar a Futuro es Testeo
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
